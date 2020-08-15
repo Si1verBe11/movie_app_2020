a71eee6a7e112ba19e641c745f9e019c48e87f88
Si1verBe11 <eunwonjong@gmail.com>
2020-08-15 16:02:47 +0900

    lecture #2.1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
-import Temp from "./temp";
+
+function Food({ fav }) {
+  return <h3>I like {fav} </h3>;
+}
 
 function App() {
   return (
     <div>
       <h1> Hello </h1>
-      <Temp />
+      <Food fav="kimchi" />
+      <Food fav="test" />
+      <Food fav="과자" />
+      <Food fav="beer" />
+      <Food fav="spam" />
     </div>
   );
 }
